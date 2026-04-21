fe49ed4ba62aa44b0cded0fd48c2a7dd11779403
Bryan <bryan@ejemplo.com>
2026-04-20 21:25:12 -0500

    Fix: excluir bin/ y obj/ del repositorio

--- obj/Debug/net10.0/GasAguaAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/GasAguaAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GasAguaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+150af7ce74831f5fc999f30ba131a9e6e6ab18cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("GasAguaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GasAguaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
